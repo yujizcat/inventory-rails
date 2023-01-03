a9b90bb2034a0592ed540ed646e92779274c57c6
yujizcat <100420400+yujizcat@users.noreply.github.com>
2023-01-03 16:04:30 +0100

    manual sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::Base
     # skip_before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    
     def after_sign_in_path_for(resource)
-        if session[:user_return_to] == nil
-            p "aaaaaa"
-          # root_path
-          redirect_to "/users/sign_out"
-        else
-          super
-        end
+        root_path
       end
 
       def after_sign_out_path_for(resource)  
