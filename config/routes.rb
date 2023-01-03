11e8937e0955af7488d8a79f6fb2faafd7c22f8e
yujizcat <100420400+yujizcat@users.noreply.github.com>
2023-01-03 15:48:21 +0100

    root to inv

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-  root to: "home#index"
-
+  # root to: "home#index"
+  root to: "inventories#index"
 
   # resources :home
   # root "articles#index"
