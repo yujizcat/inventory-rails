c1b6fc123ed2703f8603a91d9033c35d8e18a166
yujizcat <100420400+yujizcat@users.noreply.github.com>
2023-01-04 09:46:39 +0100

    users all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
     # before_action :set_user, only: [:index]
     skip_before_action :verify_authenticity_token
+    before_action :authenticate_user!
 
     def index
         @users = User.all
+        puts current_user.to_key
         render json: @users
     end
 end
